b680bc4340d15387a5306e88cd925dc028d1c2a5
felipe <felipecoutodarocha@gmail.com>
2024-05-15 20:36:45 -0300

    feat: initialized nx mono repo that will contain the microfrontend packages and their storybook

--- apps/mfe-lib/src/app/app.spec.tsx
+++ apps/mfe-lib/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome microfrontend-styleguide/gi)).toBeTruthy();
+    expect(getByText(/Welcome mfe-lib/gi)).toBeTruthy();
   });
 });
